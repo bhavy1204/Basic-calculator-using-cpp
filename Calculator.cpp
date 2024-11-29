62d1a004439ad057010142369ca222730105ecdd
bhavy1204 <bhavysarwa51@gmail.com>
2024-11-29 15:47:40 +0530

    Added more functionality

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 void manual(){
     cout<<"Welcome to manual !"<<endl;
+    cout<<"2. Enter '%' at operation for finding remainder "<<endl;
+    cout<<"1. Enter 'S' at operation for finding square root of number "<<endl;
+    cout<<"1. Enter 'A' at operation for avg "<<endl;
+    cout<<"1. Enter 'M' at operation for finding max of two numbers "<<endl;
 }
 void calc(double n1,char op,double n2){
     switch (op)
@@ -18,6 +23,24 @@ void calc(double n1,char op,double n2){
     case '/':
         cout<<n1/n2;
         break;
+    case '%':
+        cout<<int(n1)%int(n2);
+        break;
+    case 'A':
+        cout<<(n1+n2)/2.0;
+        break;
+    case 'M':
+        if (n1>=n2)
+            cout<<n1;
+        else
+            cout<<n2;
+        break;
+    case 'S':
+        cout<<"n1 :"<<sqrt(n1)<<endl;
+        cout<<"n2 :"<<sqrt(n2)<<endl;
+        break;
+    case '!':
+        break; 
     default:
         break;
     }
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Basic-calculator-using-cpp
+<h3>Simple calculator made using c++</h3>
+<p>This calculator does simple airtthemtic calculations like add,sub,multiply,divide,square root operations.</p>
